0820f40771c23481c69440a0cbd59a82ac3eda7c
shreyashChitkula <ro200020@rguktong.ac.in>
2024-01-19 15:39:54 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig
